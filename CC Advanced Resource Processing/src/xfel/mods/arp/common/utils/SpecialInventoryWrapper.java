8ab864ced2a4bd98fe31851fff645a16c74085a1
Xfel <felixtreede@yahoo.de>
2012-09-18 12:55:07 +0200

    damn you egit

---
+++ CC Advanced Resource Processing/src/xfel/mods/arp/common/utils/SpecialInventoryWrapper.java
@@ -0,0 +1,31 @@
+package xfel.mods.arp.common.utils;
+
+import net.minecraft.src.IInventory;
+import net.minecraft.src.ItemStack;
+import net.minecraftforge.common.ForgeDirection;
+import buildcraft.api.core.Orientations;
+import buildcraft.api.inventory.ISpecialInventory;
+
+public class SpecialInventoryWrapper extends InventoryWrapper implements
+		ISpecialInventory {
+
+	protected Orientations side;
+
+	public SpecialInventoryWrapper(ISpecialInventory target, ForgeDirection side) {
+		super(target);
+		this.side = Orientations.values()[side.ordinal()];
+	}
+
+	@Override
+	public int addItem(ItemStack stack, boolean doAdd, Orientations from) {
+		return ((ISpecialInventory) target).addItem(stack, doAdd, side);
+	}
+
+	@Override
+	public ItemStack[] extractItem(boolean doRemove, Orientations from,
+			int maxItemCount) {
+		return ((ISpecialInventory) target).extractItem(doRemove, side,
+				maxItemCount);
+	}
+
+}
